9c0ebdec59c8493875d4398fc1af04d0b43b437e
Morten Lied Johansen <mortenjo@ifi.uio.no>
2014-08-26 12:13:49 +0200

    Add determinant operation for Vector2

--- euclid.py
+++ euclid.py
@@ -284,6 +284,11 @@ class Vector2:
         return self.x * other.x + \
                self.y * other.y
 
+    def determinant(self, other):
+        assert isinstance(other, Vector2)
+        return self.x * other.y - \
+               self.y * other.x
+
     def cross(self):
         return Vector2(self.y, -self.x)
 
